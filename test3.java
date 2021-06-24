ea27823f4372d9498696a5ad110ec5d3bddc2204
yamikaganji <86185074+yamikaganji@users.noreply.github.com>
2021-06-24 22:49:54 +0700

    quotes within quotes

---
+++ test3.java
@@ -0,0 +1,13 @@
+package basicjavaprograms;
+
+public class test3
+{
+	
+
+		   public static void main(String args[])
+		   {
+		      System.out.println("She said \"Hello!\" to me.");
+		   
+		}
+
+}
